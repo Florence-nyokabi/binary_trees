734b04a8d00ba2003e7e698484f72a44f528099f
Florence-wangui <wanguif018@gmail.com>
2022-08-23 03:52:31 -0700

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,8 +14,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
-		return (0);
-
+	{
+		return (NULL);
+	}
 	new_node->parent = parent;
 	new_node->n = value;
 	new_node->left = NULL;
