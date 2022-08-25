f8b0cb9be3a54a841365b1d2a9dbc7485e60cab4
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-08-26 00:43:42 +0300

    Heap - Extract

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * heap_extract - extracts the root node of a Max Binary Heap
- * @root: a double pointer to the root node of heap
- *
- * Return: the value stored in the root node
- *         0 on failure
- */
+* heap_extract - function that extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of heap
+* Return: value stored in the root node
+*/
+
 int heap_extract(heap_t **root)
 {
-
+	printf("%d", (*root)->n);
+	return (0);
 }
