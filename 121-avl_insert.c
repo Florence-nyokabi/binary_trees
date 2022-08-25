c84c04dfed694dc45127f94910863fa0d67f65de
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-08-25 20:33:40 +0300

    AVL - Insert

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,71 +1,109 @@
 #include "binary_trees.h"
 
 /**
- * r_insert_node - node value instertion in a AVL.
- * @tree: type **pointer of root node of the AVL tree struct.
- * @parent: parent node of struct AVL.
- * @new: type**pointer left or right insertion.
- * @nval: insertion value of the AVL.
- * Return: pointer to the new root after insertion otherwise NULL
- */
-avl_t *r_insert_node(avl_t **tree, avl_t *parent, avl_t **new, int nval)
-{
-	int bval;
-
-	if (*tree == NULL)
-		return (*new = binary_tree_node(parent, nval));
-	if ((*tree)->n > nval)
-	{
-		(*tree)->left = r_insert_node(&(*tree)->left, *tree, new, nval);
-		if ((*tree)->left == NULL)
-			return (NULL);
-	}
-	else if ((*tree)->n < nval)
-	{
-		(*tree)->right = r_insert_node(&(*tree)->right, *tree, new, nval);
-		if ((*tree)->right == NULL)
-			return (NULL);
-	}
-	else
-	{
-		return (*tree);
-	}
-	bval = binary_tree_balance(*tree);
-	if (bval > 1 && (*tree)->left->n > nval)
-	{
-		*tree = binary_tree_rotate_right(*tree);
-	}
-	else if (bval > 1 && (*tree)->left->n < nval)
-	{
-		(*tree)->left = binary_tree_rotate_left((*tree)->left);
-		*tree = binary_tree_rotate_right(*tree);
-	}
-	else if (bval < -1 && (*tree)->right->n < nval)
-	{
-		*tree = binary_tree_rotate_left(*tree);
-	}
-	else if (bval < -1 && (*tree)->right->n > nval)
-	{
-		(*tree)->right = binary_tree_rotate_right((*tree)->right);
-		*tree = binary_tree_rotate_left(*tree);
-	}
-	return (*tree);
-}
-/**
- * avl_insert - inserts a value into an AVL tree.
- * @tree: type **pointer to the root node of the AVL tree to insert into.
- * @value: value to store in the node to be inserted
- * Return: inserted node, or NULL if fails.
+ * avl_insert - insert a new node in a Binary Search Tree
+ * @tree: a pointer to the first node
+ * @value: int value
+ *
+ * Return: If no common ancestor was found return NULL
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *new = NULL;
+	avl_t *node;
 
 	if (*tree == NULL)
 	{
 		*tree = binary_tree_node(NULL, value);
 		return (*tree);
 	}
-	r_insert_node(tree, *tree, &new, value);
-	return (new);
+
+	node = avl_recu(tree, value);
+	return (node);
+}
+
+/**
+ * avl_recu - insert a new node in a Binary Search Tree
+ * @tree: a pointer to the first node
+ * @value: int value
+ *
+ * Return: If no common ancestor was found return NULL
+ */
+avl_t *avl_recu(avl_t **tree, int value)
+{
+	avl_t *new_node;
+
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+		{
+			new_node = avl_recu(&((*tree)->left), value);
+			if (new_node)
+			{
+				balancer(tree, value);
+			}
+			return (new_node);
+		}
+	}
+	if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+		{
+			new_node = avl_recu(&((*tree)->right), value);
+			if (new_node)
+			{
+				balancer(tree, value);
+			}
+			return (new_node);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * balancer - insert a new node in a Binary Search Tree
+ * @node: a pointer to the first node
+ * @value: int value
+ *
+ * Return: If no common ancestor was found return NULL
+ */
+void balancer(avl_t **node, int value)
+{
+	int balancer;
+
+	balancer = binary_tree_balance(*node);
+
+	if (balancer > 1 && value < (*node)->left->n)
+	{
+		*node = binary_tree_rotate_right(*node);
+		return;
+	}
+	if (balancer < -1 && value > (*node)->right->n)
+	{
+		*node = binary_tree_rotate_left(*node);
+		return;
+	}
+
+	if (balancer > 1 && value > (*node)->left->n)
+	{
+		(*node)->left = binary_tree_rotate_left((*node)->left);
+		*node = binary_tree_rotate_right(*node);
+		return;
+	}
+
+	if (balancer < -1 && value < (*node)->right->n)
+	{
+		(*node)->right = binary_tree_rotate_right((*node)->right);
+		*node = binary_tree_rotate_left(*node);
+		return;
+	}
 }
