548c2b7b0fe850a1c7dbf8ed1c8a426b53e56ced
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-08-28 11:25:55 +0300

    Heap - Sort

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -1,17 +1,15 @@
 #include "binary_trees.h"
 
 /**
-* heap_to_sorted_array - function that converts a Binary Max Heap to a sorted
-* array of integers
-* @heap: pointer to the root node of the heap to convert
-* @size: address to store the size of the array
-* Return: Since we are using Max Heap, the returned array must be sorted in
-* descending order
-*/
-
+ * heap_to_sorted_array - converts a Binary Max Heap
+ * to a sorted array of integers
+ * @heap: a pointer to the root node of the heap to convert
+ * @size: an address to store the size of the array
+ *
+ * Return: the generated array
+ *         NULL on failure
+ */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
-	printf("%d", heap->n);
-	printf("%ld", *size);
-	return (0);
+
 }
